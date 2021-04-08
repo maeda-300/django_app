28eced4c792c6db9a288dfa6287ee3ee036392a9
maeda7012 <ceclass7@yahoo.co.jp>
2021-04-08 21:55:59 +0900

    P.83
    views.py編集

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,15 @@ from django.http import HttpResponse
 def index(request):
     params = {
         'title':'Hello/Index',
-        'msg':'これは、サンプルで作ったページです。'
-    }
+        'msg':'これは、サンプルで作ったページです。',
+        'goto': 'next',
+        }
+    return render(request, 'hello/index.html', params)
+
+def nex(request):
+    params = {
+        'title':'Hello/Next',
+        'msg':'これは、もう1つのページです。',
+        'goto': 'index',
+        }
     return render(request, 'hello/index.html', params)
