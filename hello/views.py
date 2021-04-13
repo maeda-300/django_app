cc3d41901541af3ef86101c17a926596de00a878
maeda7012 <ceclass7@yahoo.co.jp>
2021-04-13 22:37:58 +0900

    P.180
    views.py編集

--- hello/views.py
+++ hello/views.py
@@ -7,17 +7,8 @@ def index(request):
     data = Friend.objects.all()
     params = {
         'title': 'Hello',
-        'message': 'all friends.',
-        'form':HelloForm(),
-        'data': [],
+        'data': data,
         }
-    if (request.method == 'POST'):
-        num=request.POST['id']
-        item = Friend.objects.get(id=num)
-        params['data'] = [item]
-        params['form'] = HelloForm(request.POST)
-    else:
-        params['data'] = Friend.objects.all()
     return render(request, 'hello/index.html', params)
     
 
