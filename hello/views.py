4b2bdea07f5fa74716950aa7532e45832d677062
maeda7012 <ceclass7@yahoo.co.jp>
2021-04-13 20:40:22 +0900

    P.169
    views.py

--- hello/views.py
+++ hello/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
+from .models import Friend
 
-class HelloView(TemplateView):
-
-    def __init__(self):
-        self.params = {
-            'title': 'Hello',
-            'form': HelloForm(),
-            'result':None
+def index(request):
+    data = Friend.objects.all()
+    params = {
+        'title': 'Hello',
+        'message': 'all friends.',
+        'data': data
         }
+    return render(request, 'hello/index.html', params)
     
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
 
-    def post(self, request):
-        ch = request.POST.getlist('choice')
-        result = '<ol><b>selected:<b>'
-        for item in ch:
-            result += '<li>' + item + '</li>'
-        result += '<ol>'
-        self.params['result'] = result
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
