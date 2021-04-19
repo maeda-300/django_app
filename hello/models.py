a7600fe47294c5e545eacdedd976b6c49d7984b2
maeda7012 <ceclass7@yahoo.co.jp>
2021-04-19 20:49:14 +0900

    P.277
    models.py編集

--- hello/models.py
+++ hello/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.core.validators import URLValidator
+from django.core.validators import RegexValidator
 
 class Friend(models.Model):
     name = models.CharField(max_length=100, \
-        validators=[URLValidator()])
+        validators=[RegexValidator(r'^[a-z]*$')])
     mail = models.EmailField(max_length=200)
     gender = models.BooleanField()
     age = models.IntegerField()
