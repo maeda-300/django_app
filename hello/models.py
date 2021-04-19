0c95b73a484f995f79fe513bc6c62f644ac2b95c
maeda7012 <ceclass7@yahoo.co.jp>
2021-04-19 20:35:00 +0900

    P.274
    models.py編集

--- hello/models.py
+++ hello/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 class Friend(models.Model):
-    name = models.CharField(max_length=100)
-    mail = models.EmailField(max_length=200)
+    name = models.CharField(max_length=100, \
+        validators=[MinLengthValidator(10)])
+    mail = models.EmailField(max_length=200, \
+        validators=[MinLengthValidator(10)])
     gender = models.BooleanField()
-    age = models.IntegerField(default=0)
+    age = models.IntegerField()
     birthday = models.DateField()
 
     def __str__(self):
