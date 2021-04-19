ae5ad2d5f2a950a5402f1913663672b8ea65a477
maeda7012 <ceclass7@yahoo.co.jp>
2021-04-19 20:58:46 +0900

    P.278, P.279
    models.py編集

--- hello/models.py
+++ hello/models.py
@@ -1,9 +1,17 @@
+import re
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import ValidationError
+
+def number_only(value):
+    if (re.match(r'^[0-9]*$', value) == None):
+        raise ValidationError(
+            '%(value)s is not Number!', \
+            params={'value': value}
+        )
 
 class Friend(models.Model):
     name = models.CharField(max_length=100, \
-        validators=[RegexValidator(r'^[a-z]*$')])
+        validators=[number_only])
     mail = models.EmailField(max_length=200)
     gender = models.BooleanField()
     age = models.IntegerField()
